68771c580be477a28e17fb966c037bc9957f2c10
Your Name <you@example.com>
2022-10-05 23:43:00 +0600

    trying to run the project with separate file

--- try.js
+++ try.js
@@ -1,16 +1,49 @@
-// Options
-const CLIENT_ID = '311012207302-h28pq4siu6dn4a3lpq1spo66ebpmt5la.apps.googleusercontent.com';
-const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'];
-const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
-
-const authorizeButton = document.getElementById('authorize-button');
-const signoutButton = document.getElementById('signout-button');
 const content = document.getElementById('content');
 const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
-const defaultChannel = 'vsauce';
+/**
+ * Sample JavaScript code for youtube.playlistItems.list
+ * See instructions for running APIs Explorer code samples locally:
+ * https://developers.google.com/explorer-help/code-samples#javascript
+ */
+
+
+//authenticate google sign in
+function authenticate() {
+return gapi.auth2.getAuthInstance()
+    .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+    .then(function() { console.log("Sign-in successful"); },
+            function(err) { console.error("Error signing in", err); });
+}
+function loadClient() {
+gapi.client.setApiKey("AIzaSyBApTDIWjTq1piLbGqFRCXshiL8v8BuCYc");
+return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+    .then(function() { console.log("GAPI client loaded for API"); },
+            function(err) { console.error("Error loading GAPI client for API", err); });
+}
+
+
+// // Make sure the client is loaded and sign-in is complete before calling this method.
+// function execute() {
+// return gapi.client.youtube.channels.list({
+//     part: 'snippet,contentDetails,statistics',
+//     forUsername: 'vsauce'
+// })
+//     .then(function(response) {
+//             // Handle the results here (response.result has the parsed body).
+//             console.log("Response", response);
+//             },
+//             function(err) { console.error("Execute error", err); });
+// }
+
+
+gapi.load("client:auth2", function() {
+gapi.auth2.init({client_id: "311012207302-h28pq4siu6dn4a3lpq1spo66ebpmt5la.apps.googleusercontent.com"});
+});
+
+
 
 // Form submit and change channel
 channelForm.addEventListener('submit', e => {
@@ -20,55 +53,6 @@ channelForm.addEventListener('submit', e => {
 
   getChannel(channel);
 });
-
-// // Load auth2 library
-// function handleClientLoad() {
-//   gapi.load('client:auth2', initClient);
-// }
-
-// // Init API client library and set up sign in listeners
-// function initClient() {
-//   gapi.client.init({
-//       discoveryDocs: DISCOVERY_DOCS,
-//       clientId: CLIENT_ID,
-//       scope: SCOPES
-//     })
-//     .then(() => {
-//       // Listen for sign in state changes
-//       gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-//       // Handle initial sign in state
-//       updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-//       authorizeButton.onclick = handleAuthClick;
-//       signoutButton.onclick = handleSignoutClick;
-//     });
-// }
-
-// // Update UI sign in state changes
-// function updateSigninStatus(isSignedIn) {
-//   if (isSignedIn) {
-//     authorizeButton.style.display = 'none';
-//     signoutButton.style.display = 'block';
-//     content.style.display = 'block';
-//     videoContainer.style.display = 'block';
-//     getChannel(defaultChannel);
-//   } else {
-//     authorizeButton.style.display = 'block';
-//     signoutButton.style.display = 'none';
-//     content.style.display = 'none';
-//     videoContainer.style.display = 'none';
-//   }
-// }
-
-// // Handle login
-// function handleAuthClick() {
-//   gapi.auth2.getAuthInstance().signIn();
-  
-// }
-
-// // Handle logout
-// function handleSignoutClick() {
-//   gapi.auth2.getAuthInstance().signOut();
-// }
 
 // Display channel data
 function showChannelData(data) {
@@ -121,6 +105,7 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
+//doing something
 function requestVideoPlaylist(playlistId) {
   const requestOptions = {
     playlistId: playlistId,
